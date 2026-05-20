fd98a2e09c5aa3fbf6c08cd5bd1299fb11ec7148
Philipp Comans <philipp.comans@gmail.com>
2026-05-20 15:53:39 -0700

    Update zelligent to v0.2.3

--- Formula/zelligent.rb
+++ Formula/zelligent.rb
@@ -1,9 +1,9 @@
 class Zelligent < Formula
   desc "Spawn AI coding agents in Zellij worktree tabs"
   homepage "https://github.com/pcomans/zelligent"
-  version "0.2.2"
+  version "0.2.3"
   url "https://github.com/pcomans/zelligent/releases/download/v#{version}/zelligent-v#{version}.tar.gz"
-  sha256 "9f066490e3ee377c07d58912bf5088972e4db35d3fa56e1eef5744d19fbc2748"
+  sha256 "22ea8b0e8b316ad8500cc32acbf87e60ca226157c8b09744d2a3b2b3838a84e3"
   license "MIT"
 
   depends_on "zellij"
